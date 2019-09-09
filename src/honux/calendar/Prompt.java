77711598521d87b9beb20f7e19f2f5c2cd4e5892
yejin <yejin@192.168.25.10>
2019-09-09 19:09:54 +0900

    hahahaha

--- src/honux/calendar/Prompt.java
+++ src/honux/calendar/Prompt.java
@@ -3,15 +3,33 @@ package honux.calendar;
 import java.util.Scanner;
 
 public class Prompt {
-	private final static String PROMPT="cal>";
-	
+	/**
+	 * 
+	 * @param week 요일명
+	 * @return 0~6(0= 일요일, 6= 토요일)
+	 */
+
+	public int parseDay (String week) {
+		if(week.equals("일")) return 0;
+		
+		else if (week .equals("월"))return 1;
+        else if (week .equals("화"))return 2;
+		else if (week .equals("수"))return 3;
+		else if (week .equals("목"))return 4;
+		else if (week .equals("금"))return 5;
+		else if (week .equals("토"))return 6;
+		else
+			return 0;
+								
+	}
 
 	public void runPrompt() {
 		Scanner scanner = new Scanner(System.in);
 		calendar cal = new calendar();
 		
 		int month= -1;
-		int year =-1;
+		int year =2019;
+		int weekday =0;
 
 		while (true) {	
 			
@@ -21,13 +39,17 @@ public class Prompt {
 		System.out.println("달을 입력하세요");
 		System.out.println("MONTH>");
 		month = scanner.nextInt();
+		System.out.println("첫째 날의 요일을 입력하세요(일, 월, 화, 수, 목, 금, 토).");
+		String str_weekday = scanner.next();
+		weekday = parseDay(str_weekday);
+		
 		if(month==-1) {
 break;
 		}
 		if(month >12) {
 			continue;
 		}
-		cal.printCalendar(year, month);
+		cal.printCalendar(year, month, weekday);
 		}
 
 		scanner.close();
