55050d9c07ea5dfa4ab0bfa4c42c58432fa1e211
yejin <yejin@192.168.25.10>
2019-09-09 17:44:48 +0900

    update calendar calculation

--- src/honux/calendar/calendar.java
+++ src/honux/calendar/calendar.java
@@ -9,13 +9,23 @@ public class calendar {
 		return MAX_DAYS[month-1];
 		
 	}
-	public static void main(String[] args) {
+	public void printCalendar(int year, int month) {
+		System.out.printf("  <<%4d  %3d>>\n", year, month);
 System.out.println("일   월   화   수   목   금   토");
 System.out.println("---------------------");
-System.out.println(" 1  2  3  4  5  6  7 ");
-System.out.println(" 8  9 10 11 12 13 14");
-System.out.println("15 16 17 18 19 20 21");
-System.out.println("22 23 24 25 26 27 28");
+
+int maxDay = getmaxDaysOFMonth(month);
+
+for(int i=1; i<= maxDay; i++) {
+  System.out.printf("%3d",i);
+  if(i %7==0)
+	  System.out.println();
+}
+//System.out.println();
+//System.out.println(" 1  2  3  4  5  6  7 ");
+//System.out.println(" 8  9 10 11 12 13 14");
+//System.out.println("15 16 17 18 19 20 21");
+//System.out.println("22 23 24 25 26 27 28");
 
 
 //숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
@@ -24,6 +34,7 @@ String PROMPT="cal>";
 Scanner scanner = new Scanner(System.in);
 calendar cal = new calendar();
 
+System.out.println();
 System.out.println("입력횟수를 입력하세요");
 int repeat = scanner.nextInt();
 
@@ -31,7 +42,8 @@ for(int i=0; i< repeat; i++) {
 
 System.out.println("달을 입력하세요");
 System.out.println(PROMPT);
-int month = scanner.nextInt();
+month = scanner.nextInt();
+
 
 System.out.printf("%d월은 %d일까지 있습니다.", month, cal.getmaxDaysOFMonth(month));
 }
