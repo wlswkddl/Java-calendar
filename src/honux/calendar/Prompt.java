62f674e1ab1d65d414cc55809f806d98ddd15f36
yejin <yejin@192.168.25.10>
2019-09-09 18:13:36 +0900

    YEAR MONTH

--- src/honux/calendar/Prompt.java
+++ src/honux/calendar/Prompt.java
@@ -9,13 +9,17 @@ public class Prompt {
 	public void runPrompt() {
 		Scanner scanner = new Scanner(System.in);
 		calendar cal = new calendar();
-
-	
-		int month = 1;
+		
+		int month= -1;
+		int year =-1;
 
 		while (true) {	
+			
+		System.out.println("년도를 입력하세요");
+		System.out.println("YEAR>");
+		year= scanner.nextInt();
 		System.out.println("달을 입력하세요");
-		System.out.println(PROMPT);
+		System.out.println("MONTH>");
 		month = scanner.nextInt();
 		if(month==-1) {
 break;
@@ -23,7 +27,7 @@ break;
 		if(month >12) {
 			continue;
 		}
-		cal.printCalendar(2019, month);
+		cal.printCalendar(year, month);
 		}
 
 		scanner.close();
