b3a85963fc309738a8286edff51a7c7a46ef7a31
yejin <yejin@192.168.25.10>
2019-09-09 16:51:39 +0900

    update days repeat

--- src/honux/calendar/calendar.java
+++ src/honux/calendar/calendar.java
@@ -19,16 +19,24 @@ System.out.println("22 23 24 25 26 27 28");
 
 
 //숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
+
+String PROMPT="cal>";
 Scanner scanner = new Scanner(System.in);
 calendar cal = new calendar();
 
+System.out.println("입력횟수를 입력하세요");
+int repeat = scanner.nextInt();
+
+for(int i=0; i< repeat; i++) {
+
 System.out.println("달을 입력하세요");
+System.out.println(PROMPT);
 int month = scanner.nextInt();
 
-int[] maxDays = {31, 28, 31,30,31,30,31,31,30,31,30,31};
-
 System.out.printf("%d월은 %d일까지 있습니다.", month, cal.getmaxDaysOFMonth(month));
-scanner.close();
-	}
+}
 
+scanner.close();
+	
+	}
 }
